a8e3d31c02f52593f74babe57c494fa5670392ec
blakebdavies <81705490+blakebdavies@users.noreply.github.com>
2021-06-02 20:06:17 +0100

    Created dictionary component and search functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
-import './App.css';
 import logo from "./logo.png";
+import './App.css';
+
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={logo} className="App-logo img-fluid" alt="logo" />
       </header>
+      <main>
+      <Dictionary />
+      </main>
+      </div>
+      <footer className="App-footer"><small>Coded by Blake Davies</small></footer>
     </div>
   );
 }
