e9ec5d301f1be78ee878f3f8176108399698b8d1
blakebdavies <81705490+blakebdavies@users.noreply.github.com>
2021-06-04 21:04:12 +0100

    Styled the application

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ export default function App() {
         <img src={logo} className="App-logo img-fluid" alt="logo" />
       </header>
       <main>
-      <Dictionary />
+      <Dictionary defaultKeyword= "hygge" />
       </main>
       </div>
       <footer className="App-footer"><small>Coded by Blake Davies</small></footer>
