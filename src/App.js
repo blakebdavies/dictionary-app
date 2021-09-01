93fc39681af2381e884dfb0b472f530788d77abd
blakebdavies <81705490+blakebdavies@users.noreply.github.com>
2021-09-01 17:44:44 +0100

    updated links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "./logo.png";
-import './App.css';
+import "./App.css";
 
 import Dictionary from "./Dictionary";
 
@@ -8,15 +8,35 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
-      <main>
-      <Dictionary defaultKeyword= "wanderlust" />
-      </main>
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary defaultKeyword="wanderlust" />
+        </main>
       </div>
       <footer className="App-footer">
-        <small>This project was coded by Blake Davies, is <a href="https://github.com/blakebdavies/dictionary-app" target="_blank" rel="noreferrer">open-sourced</a> and hosted on <a href="https://eloquent-babbage-8e1acc.netlify.app" target="_blank" rel="noreferrer">Netlify</a></small></footer>
+        <small>
+          This project was coded by Blake Davies, is{" "}
+          <a
+            href="https://github.com/blakebdavies/dictionary-app"
+            target="_blank"
+            rel="noreferrer"
+            title="View Dictionary GitHub"
+          >
+            open-sourced
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://eloquent-babbage-8e1acc.netlify.app"
+            target="_blank"
+            rel="noreferrer"
+            title="Launch Dictionary App"
+          >
+            Netlify
+          </a>
+        </small>
+      </footer>
     </div>
   );
 }
